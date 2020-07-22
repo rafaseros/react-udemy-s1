ca5e706c8ed4b979cb749ec25594e2f77a516e96
devrafaseros <devrafaseros@gmail.com>
2020-07-22 14:51:53 -0400

    javascript objects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-const name = 'Rafael';
-const lastName = 'Gallegos';
+const person = {
+  name: 'Rafael',
+  lastname: 'Gallegos',
+  age: 29,
+  adreess: {
+    city: 'Santa Cruz',
+    town: 'El triunfo',
+    zone: 'El palmar',
+    uv: 174,
+    mz: 14,
+  },
+};
 
-const fullName = `${name} ${lastName}`;
+const person2 = { ...person };
+person2.name = 'Jose';
 
-console.log(fullName);
+console.table(person);
+console.table(person2);
