ca61975524ee038fa4f17af1d7544c59dd379b80
devrafaseros <devrafaseros@gmail.com>
2020-07-22 17:47:27 -0400

    Ternario

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const getGif = async () => {
-  try {
-    const API_KEY = 'KzBUHGTAFsK94lN0hRVHnJcSFG4B58Y7';
-    const res = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
-    const { data } = await res.json();
-    const { url } = data.images.original;
-    const img = document.createElement('img');
-    img.src = url;
-    document.body.append(img);
-  } catch (err) {
-    console.error(err);
-  }
-};
+const activo = true;
 
-getGif();
+// let message = '';
+
+// if (activo) {
+//   message = 'Activo';
+// } else {
+//   message = 'Inactivo';
+// }
+
+// const message = activo ? 'Activo' : 'Inactivo';
+
+const message = !activo && 'activo';
+
+console.log(message)
