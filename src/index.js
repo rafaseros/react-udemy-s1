ef4d245e78a1ad43a5a1c08a61d86a0fa4b06aad
devrafaseros <devrafaseros@gmail.com>
2020-07-22 16:29:35 -0400

    imports part 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-const { heroes } = require('./data/heroes');
+import heroes, { owners } from './data/heroes';
+
+console.log(owners);
 
 const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
+
 const getHeroeByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
 
 console.log(getHeroeById(2));
+
 console.log(getHeroeByOwner('DC'));
