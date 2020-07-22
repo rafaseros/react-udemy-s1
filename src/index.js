743a0da4a555f02e56015ccbebc9fb452d06be9c
devrafaseros <devrafaseros@gmail.com>
2020-07-22 14:39:58 -0400

    Template Strings

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-const name = "Rafael";
-const lastName = "Gallegos";
+const name = 'Rafael';
+const lastName = 'Gallegos';
 
-let valorDado = 5;
-valorDado = 4;
+const fullName = `${name} ${lastName}`;
 
-if (true) {
-  let valorDado = 3;
-  console.log(valorDado);
-}
-
-valorDado = 10;
-
-console.log(name, lastName, valorDado);
+console.log(fullName);
