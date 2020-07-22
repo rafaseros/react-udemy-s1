97ed80d95580edfbee5a46365bb7f23c00a3324c
devrafaseros <devrafaseros@gmail.com>
2020-07-22 17:39:04 -0400

    async await

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-const API_KEY = 'KzBUHGTAFsK94lN0hRVHnJcSFG4B58Y7';
-const HTTP_CALL = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
-
-HTTP_CALL.then((res) => res.json())
-  .then(({ data }) => {
+const getGif = async () => {
+  try {
+    const API_KEY = 'KzBUHGTAFsK94lN0hRVHnJcSFG4B58Y7';
+    const res = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
+    const { data } = await res.json();
     const { url } = data.images.original;
     const img = document.createElement('img');
     img.src = url;
     document.body.append(img);
-  })
-  .catch(console.warn);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getGif();
